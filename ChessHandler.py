c746d6ad8b77a14a75f79f3f8c06307db454400c
notxeroc <danielgriffor@gmail.com>
2026-02-19 19:21:42 -0500

    Change HumanChessBot to ComplexChessBot

--- ChessHandler.py
+++ ChessHandler.py
@@ -4,12 +4,12 @@ import chess.pgn
 from ChessGUI import chessGUI
 
 import SimpleChessBot
-import HumanChessBot
+import ComplexChessBot
 import RandomChessBot
 import CheckChessBot
 
-white_bot = HumanChessBot.Bot(chess.WHITE, depth=3)
-black_bot = HumanChessBot.Bot(chess.BLACK, depth=3)
+white_bot = ComplexChessBot.Bot(chess.WHITE, depth=3)
+black_bot = ComplexChessBot.Bot(chess.BLACK, depth=3)
 
 gui = chessGUI(white_player=white_bot, black_player=black_bot)
 gui.move_time = 100
