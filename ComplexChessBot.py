5bc8e65d8444b1236b3ec3c6e6ed3e6212889c80
danielgriffor-cmyk <danielgriffor@gmail.com>
2026-02-20 13:00:41 -0500

    Update Complex Chess Bot Logic

--- ComplexChessBot.py
+++ ComplexChessBot.py
@@ -7,7 +7,7 @@ class Bot(ChessBotBase.Bot):
     def evaluate(self, board):
         
         if board.is_checkmate():
-            return -1e6 if board.turn == self.color else 1e6
+            return -math.inf if board.turn == self.color else math.inf
         
         score = 0
         pawn_val, knight_val, bishop_val, rook_val, queen_val = 10, 30, 35, 55, 100
